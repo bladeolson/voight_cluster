7e02093cb4696b3cd851c6658c05f1f5c8ff1a77
Blade Olson <bladeolson@example.com>
2025-12-15 00:45:39 -0800

    Ghost robot: shift up more (-720px) to center eyes

--- kokoro/zen_kokoro_server.py
+++ kokoro/zen_kokoro_server.py
@@ -1585,7 +1585,7 @@ async def dashboard(request: Request):
         .zen-eyes-container::before {
             content: '';
             position: fixed;
-            top: -580px;
+            top: -720px;
             left: 50%;
             transform: translateX(-50%);
             width: 7000px;
